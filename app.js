a46e5792b68a710237b72167c9b480240e1af1bb
Ravindu Dilshan <90592440+ravindudil5han@users.noreply.github.com>
2023-01-26 16:54:42 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const request = require('request');
+let previousResponse;
+
+const checkAPI = () => {
+  request('api key ', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      if (previousResponse !== body) {
+        console.log(body);
+        previousResponse = body;
+      }
+    } else {
+      console.log(`Error: ${error}`);
+      console.log(`Status code: ${response.statusCode}`);
+    }
+  });
+}
+
+setInterval(checkAPI, 60000); // check the API every minute (60000 milliseconds)
